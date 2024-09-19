5319d522bb4532fc232ff664280fa7552cf714ee
KennyDiff <19239945+kennydiff@users.noreply.github.com>
2024-09-19 15:37:42 +0800

    1

--- client/lib/features/auth/view/pages/login_page.dart
+++ client/lib/features/auth/view/pages/login_page.dart
@@ -10,125 +10,123 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginPage extends ConsumerStatefulWidget {
-  const LoginPage({super.key});
+    const LoginPage({super.key});
 
-  @override
-  ConsumerState<LoginPage> createState() => _LoginPageState();
+    @override
+    ConsumerState<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends ConsumerState<LoginPage> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final formKey = GlobalKey<FormState>();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
 
-  @override
-  void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
+    @override
+    void dispose() {
+        emailController.dispose();
+        passwordController.dispose();
+        super.dispose();
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    final isLoading = ref
-        .watch(authViewModelProvider.select((val) => val?.isLoading == true));
+    @override
+    Widget build(BuildContext context) {
+        final isLoading = ref.watch(authViewModelProvider.select((val) => val?.isLoading == true));
 
-    ref.listen(
-      authViewModelProvider,
-      (_, next) {
-        next?.when(
-          data: (data) {
-            Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const HomePage(),
-              ),
-              (_) => false,
-            );
-          },
-          error: (error, st) {
-            showSnackBar(context, error.toString());
-          },
-          loading: () {},
-        );
-      },
-    );
-
-    return Scaffold(
-      appBar: AppBar(),
-      body: isLoading
-          ? const Loader()
-          : Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Sign In.',
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    CustomField(
-                      hintText: 'Email',
-                      controller: emailController,
-                    ),
-                    const SizedBox(height: 15),
-                    CustomField(
-                      hintText: 'Password',
-                      controller: passwordController,
-                      isObscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    AuthGradientButton(
-                      buttonText: 'Sign in',
-                      onTap: () async {
-                        if (formKey.currentState!.validate()) {
-                          await ref
-                              .read(authViewModelProvider.notifier)
-                              .loginUser(
-                                email: emailController.text,
-                                password: passwordController.text,
-                              );
-                        } else {
-                          showSnackBar(context, 'Missing fields!');
-                        }
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SignupPage(),
-                          ),
-                        );
-                      },
-                      child: RichText(
-                        text: TextSpan(
-                          text: 'Don\'t have an account? ',
-                          style: Theme.of(context).textTheme.titleMedium,
-                          children: const [
-                            TextSpan(
-                              text: 'Sign Up',
-                              style: TextStyle(
-                                color: Pallete.gradient2,
-                                fontWeight: FontWeight.bold,
-                              ),
+        ref.listen(
+            authViewModelProvider,
+            (_, next) {
+                next?.when(
+                    data: (data) {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomePage(),
                             ),
-                          ],
+                            (_) => false,
+                        );
+                    },
+                    error: (error, st) {
+                        showSnackBar(context, error.toString());
+                    },
+                    loading: () {},
+                );
+            },
+        );
+
+        return Scaffold(
+            appBar: AppBar(),
+            body: isLoading
+                ? const Loader()
+                : Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Form(
+                            key: formKey,
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                    const Text(
+                                        '登录',
+                                        style: TextStyle(
+                                            fontSize: 50,
+                                            fontWeight: FontWeight.bold,
+                                        ),
+                                    ),
+                                    const SizedBox(height: 30),
+                                    CustomField(
+                                        hintText: '邮箱',
+                                        controller: emailController,
+                                    ),
+                                    const SizedBox(height: 15),
+                                    CustomField(
+                                        hintText: '密码',
+                                        controller: passwordController,
+                                        isObscureText: true,
+                                    ),
+                                    const SizedBox(height: 20),
+                                    AuthGradientButton(
+                                        buttonText: '登录',
+                                        onTap: () async {
+                                            if (formKey.currentState!.validate()) {
+                                                await ref.read(authViewModelProvider.notifier).loginUser(
+                                                        email: emailController.text,
+                                                        password: passwordController.text,
+                                                    );
+                                            } else {
+                                                showSnackBar(context, 'Missing fields!');
+                                            }
+                                        },
+                                    ),
+                                    const SizedBox(height: 20),
+                                    GestureDetector(
+                                        onTap: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) => const SignupPage(),
+                                                ),
+                                            );
+                                        },
+                                        child: RichText(
+                                            text: TextSpan(
+                                                // text: 'Don\'t have an account? ',
+                                                text: '还没有账号?  ',
+                                                style: Theme.of(context).textTheme.titleMedium,
+                                                children: const [
+                                                    TextSpan(
+                                                        text: '注册',
+                                                        style: TextStyle(
+                                                            color: Pallete.gradient2,
+                                                            fontWeight: FontWeight.bold,
+                                                        ),
+                                                    ),
+                                                ],
+                                            ),
+                                        ),
+                                    )
+                                ],
+                            ),
                         ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-    );
-  }
+                    ),
+        );
+    }
 }
--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -9,136 +9,136 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SignupPage extends ConsumerStatefulWidget {
-  const SignupPage({super.key});
+    const SignupPage({super.key});
 
-  @override
-  ConsumerState<SignupPage> createState() => _SignupPageState();
+    @override
+    ConsumerState<SignupPage> createState() => _SignupPageState();
 }
 
 class _SignupPageState extends ConsumerState<SignupPage> {
-  final nameController = TextEditingController();
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final formKey = GlobalKey<FormState>();
+    final nameController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
 
-  @override
-  void dispose() {
-    nameController.dispose();
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
+    @override
+    void dispose() {
+        nameController.dispose();
+        emailController.dispose();
+        passwordController.dispose();
+        super.dispose();
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    final isLoading = ref
-        .watch(authViewModelProvider.select((val) => val?.isLoading == true));
+    @override
+    Widget build(BuildContext context) {
+        final isLoading = ref.watch(authViewModelProvider.select((val) => val?.isLoading == true));
 
-    ref.listen(
-      authViewModelProvider,
-      (_, next) {
-        next?.when(
-          data: (data) {
-            showSnackBar(
-              context,
-              'Account created successfully! Please  login.',
-            );
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const LoginPage(),
-              ),
-            );
-          },
-          error: (error, st) {
-            showSnackBar(context, error.toString());
-          },
-          loading: () {},
-        );
-      },
-    );
-
-    return Scaffold(
-      appBar: AppBar(),
-      body: isLoading
-          ? const Loader()
-          : Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Sign Up.',
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    CustomField(
-                      hintText: 'Name',
-                      controller: nameController,
-                    ),
-                    const SizedBox(height: 15),
-                    CustomField(
-                      hintText: 'Email',
-                      controller: emailController,
-                    ),
-                    const SizedBox(height: 15),
-                    CustomField(
-                      hintText: 'Password',
-                      controller: passwordController,
-                      isObscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    AuthGradientButton(
-                      buttonText: 'Sign up',
-                      onTap: () async {
-                        if (formKey.currentState!.validate()) {
-                          await ref
-                              .read(authViewModelProvider.notifier)
-                              .signUpUser(
-                                name: nameController.text,
-                                email: emailController.text,
-                                password: passwordController.text,
-                              );
-                        } else {
-                          showSnackBar(context, 'Missing fields!');
-                        }
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginPage(),
-                          ),
+        ref.listen(
+            authViewModelProvider,
+            (_, next) {
+                next?.when(
+                    data: (data) {
+                        showSnackBar(
+                            context,
+                            'Account created successfully! Please  login.',
                         );
-                      },
-                      child: RichText(
-                        text: TextSpan(
-                          text: 'Already have an account? ',
-                          style: Theme.of(context).textTheme.titleMedium,
-                          children: const [
-                            TextSpan(
-                              text: 'Sign In',
-                              style: TextStyle(
-                                color: Pallete.gradient2,
-                                fontWeight: FontWeight.bold,
-                              ),
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginPage(),
                             ),
-                          ],
+                        );
+                    },
+                    error: (error, st) {
+                        showSnackBar(context, error.toString());
+                    },
+                    loading: () {},
+                );
+            },
+        );
+
+        return Scaffold(
+            appBar: AppBar(),
+            body: isLoading
+                ? const Loader()
+                : Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        // K_24919 child 单数，一个成员
+                        child: Form(
+                            key: formKey,
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                // K_24919 children 复数，多个成员
+                                children: [
+                                    const Text(
+                                        '注册',
+                                        style: TextStyle(
+                                            fontSize: 50,
+                                            fontWeight: FontWeight.bold,
+                                        ),
+                                    ),
+                                    const SizedBox(height: 30),
+                                    CustomField(
+                                        hintText: '姓名',
+                                        controller: nameController,
+                                    ),
+                                    const SizedBox(height: 15),
+                                    CustomField(
+                                        hintText: '邮箱',
+                                        controller: emailController,
+                                    ),
+                                    const SizedBox(height: 15),
+                                    CustomField(
+                                        hintText: '密码',
+                                        controller: passwordController,
+                                        isObscureText: true,
+                                    ),
+                                    const SizedBox(height: 20),
+                                    AuthGradientButton(
+                                        buttonText: '注册',
+                                        onTap: () async {
+                                            if (formKey.currentState!.validate()) {
+                                                await ref.read(authViewModelProvider.notifier).signUpUser(
+                                                        name: nameController.text,
+                                                        email: emailController.text,
+                                                        password: passwordController.text,
+                                                    );
+                                            } else {
+                                                showSnackBar(context, 'Missing fields!');
+                                            }
+                                        },
+                                    ),
+                                    const SizedBox(height: 20),
+                                    GestureDetector(
+                                        onTap: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) => const LoginPage(),
+                                                ),
+                                            );
+                                        },
+                                        child: RichText(
+                                            text: TextSpan(
+                                                // text: 'Already have an account? ',
+                                                text: '已经有账号了?  ',
+                                                style: Theme.of(context).textTheme.titleMedium,
+                                                children: const [
+                                                    TextSpan(
+                                                        text: '登录',
+                                                        style: TextStyle(
+                                                            color: Pallete.gradient2,
+                                                            fontWeight: FontWeight.bold,
+                                                        ),
+                                                    ),
+                                                ],
+                                            ),
+                                        ),
+                                    )
+                                ],
+                            ),
                         ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-    );
-  }
+                    ),
+        );
+    }
 }
